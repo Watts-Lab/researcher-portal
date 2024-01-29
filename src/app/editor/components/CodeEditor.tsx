483d709891068ba52ffd22b132c3feb3af64b7d2
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2024-01-29 16:55:55 -0500

    add TODO comment

--- src/app/editor/components/CodeEditor.tsx
+++ src/app/editor/components/CodeEditor.tsx
@@ -34,6 +34,7 @@ export default function CodeEditor() {
       localStorage.setItem("code", entry);
     } catch (YAMLParseError) {
       //TODO also display a little something went wrong pop up
+      //TODO find a way for changes to still appear and just not parse it - might need to happen in timeline
     }
     window.location.reload(false) //refresh page to make elements appear on screen
   }
