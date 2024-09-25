a3a467c9745ad9d59546205c43b2262d096bac70
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2024-09-25 10:52:06 -0400

    Revert "experiment with styling"
    This reverts commit 7da46c35c766e5a7593f24ecb750e3f89982c2e8.

--- src/app/editor/elements/TrainingVideo.tsx
+++ src/app/editor/elements/TrainingVideo.tsx
@@ -7,21 +7,4 @@ export default function SubmitButton({ url } : { url : any}) {
       <p> url: {url} </p>
     </div>
   );
-    // return (
-    // <div style={{ position: 'relative', width: '100%', paddingTop: '70%'}}>
-    //   <h1> Training Video </h1>
-    //   <div style={{ position: 'absolute', top: 0, left: 0, width: '90%', height: '90%' }}>
-    //     <p> url: {url} </p>
-    //   </div>
-    // </div>
-    // );
 }
-
-// case "video":
-//   return (
-//     <div style={{ position: 'relative', width: '100%', paddingTop: '70%'}}>
-//       <div style={{ position: 'absolute', top: 0, left: 0, width: '90%', height: '90%' }}>
-//         <TrainingVideo url={element.url}/>
-//       </div>
-//     </div>
-//   );
