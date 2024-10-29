61b6f6124ba9f37c6ef510179a8be2ab73f52a5a
Dan <dankim1@seas.upenn.edu>
2024-10-29 13:26:25 -0400

    add new tests

--- cypress/e2e/filterTreatment.cy.ts
+++ cypress/e2e/filterTreatment.cy.ts
@@ -25,29 +25,62 @@ describe('timeline filter treatment', () => {
         cy.get('[data-cy="edit-stage-duration-new"]').type("{backspace}200");
         cy.get('[data-cy="edit-stage-save-new"]').click();
 
+        // all sections should initially be visible
         cy.get('[data-cy="stage-0"]').contains("Role Assignment and General Instructions").should("be.visible");
         cy.get('[data-cy="stage-1"]').contains("Main Discussion").should("be.visible");
         cy.get('[data-cy="stage-2"]').contains("Post Discussion Survey").should("be.visible");
+        cy.get('[data-cy^="stage-"]').should('have.length', 3);
     });
 
+    it('should dynamically populate filter options based on treatment file', () => {
+        cy.get('[data-cy="filter-dropdown"] select').within(() => {
+            cy.get('option').should('have.length', 4);
+            cy.get('option').eq(0).should('have.text', 'All Stages');
+            cy.get('option').eq(1).should('have.text', 'Role Assignment and General Instructions');
+            cy.get('option').eq(2).should('have.text', 'Main Discussion');
+            cy.get('option').eq(3).should('have.text', 'Post Discussion Survey');
+        });
+    })
+
     it('allows filtering stages by criteria', () => {
+        // default option
+        cy.get('[data-cy="filter-dropdown"] select').should('have.value', 'all');
+
         // select one section and ensure no other sections are visible
+        cy.get('[data-cy="filter-dropdown"] select').select('Main Discussion');
+        cy.get('[data-cy="filter-dropdown"] select').should('have.value', 'Main Discussion');
+        cy.get('[data-cy="stage-0"]').should('not.exist');
+        cy.get('[data-cy="stage-2"]').should('not.exist');
+        cy.get('[data-cy="stage-1"]').contains("Main Discussion").should("be.visible");
+        cy.get('[data-cy^="stage-"]').should('have.length', 1);
+
+        // reset to all stages when filter option is cleared
+        cy.get('[data-cy="filter-dropdown"] select').select('all');
+        cy.get('[data-cy="filter-dropdown"] select').should('have.value', 'all');
+        cy.get('[data-cy="stage-0"]').contains("Role Assignment and General Instructions").should("be.visible");
+        cy.get('[data-cy="stage-1"]').contains("Main Discussion").should("be.visible");
+        cy.get('[data-cy="stage-2"]').contains("Post Discussion Survey").should("be.visible");
+        cy.get('[data-cy^="stage-"]').should('have.length', 3);
+    })
+
+    it('should persist selected filter after page reload', () => {
+        cy.get('[data-cy="filter-dropdown"] select').should('have.value', 'all');
+
         cy.get('[data-cy="filter-dropdown"] select').select('Main Discussion');
         cy.get('[data-cy="stage-0"]').should('not.exist');
         cy.get('[data-cy="stage-2"]').should('not.exist');
         cy.get('[data-cy="stage-1"]').contains("Main Discussion").should("be.visible");
+        cy.get('[data-cy^="stage-"]').should('have.length', 1);
 
-        // reset filter to show all stages
-        cy.get('[data-cy="filter-dropdown"] select').select('all');
-        cy.get('[data-cy="stage-0"]').contains("Role Assignment and General Instructions").should("be.visible");
+        cy.reload(); // reload page
+
+        cy.get('[data-cy="filter-dropdown"] select').select('Main Discussion');
+        cy.get('[data-cy="filter-dropdown"] select').should('have.value', 'Main Discussion');
+        cy.get('[data-cy="stage-0"]').should('not.exist');
+        cy.get('[data-cy="stage-2"]').should('not.exist');
         cy.get('[data-cy="stage-1"]').contains("Main Discussion").should("be.visible");
-        cy.get('[data-cy="stage-2"]').contains("Post Discussion Survey").should("be.visible");
+        cy.get('[data-cy^="stage-"]').should('have.length', 1);
     })
 
-    // it('should dynamically populate filter options based on treatment file', () => {
-
-    // })
-
-    // it('should persist selected filter after page reload', () => {
-    // })
+    // add test for same named stages
 })
