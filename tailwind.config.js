38c6ea6318392c093edfde743e4620bce9efd555
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2024-11-05 15:24:42 -0500

    delete comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,3 @@
-// import type { Config } from "tailwindcss";
-// const plugin = require("tailwindcss/plugin");
-
-// const config: Config = {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       backgroundImage: {
-//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-//         "gradient-conic":
-//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-//       },
-//     },
-//   },
-//   plugins: [
-//     require("daisyui"),
-//     plugin(function ({ addBase, theme }) {
-//       addBase({
-//         h1: { fontSize: theme("fontSize.2xl") },
-//         h2: { fontSize: theme("fontSize.xl") },
-//         h3: { fontSize: theme("fontSize.lg") },
-//       });
-//     }),
-//   ],
-
-//   daisyui: {
-//     themes: ["cupcake"],
-//   },
-// };
-// export default config;
-
-
 import type { Config } from "tailwindcss";
 const plugin = require("tailwindcss/plugin");
 
