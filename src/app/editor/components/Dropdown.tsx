3289be72832ed1f369a62fb7784c3543b73a9766
Dan <dankim1@seas.upenn.edu>
2024-12-30 20:06:58 -0500

    enhance dropdown component to handle empty options gracefully

--- src/app/editor/components/Dropdown.tsx
+++ src/app/editor/components/Dropdown.tsx
@@ -15,27 +15,36 @@ const Dropdown = ({
   onChange,
   dataCy,
 }: DropdownProps) => {
+  const isStageOptionsEmpty = options.length === 1 && options[0] === 'all'
+
   return (
     <div data-cy={dataCy} className="flex items-center space-x-2">
       <label className="text-gray font-medium">{label}</label>
       <select
         value={value}
         onChange={onChange}
-        className="select select-bordered bg-white text-gray-700 font-medium h-full text-sm rounded-md border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 truncate max-w-xs"
+        className="select select-bordered bg-white text-gray-700 font-medium h-full text-sm rounded-md border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 truncate"
+        style={{ width: '200px' }} // Fixed width
       >
-        {options.map((option, index) => (
-          <option
-            key={index}
-            value={typeof value === 'number' ? index : option}
-            className="truncate"
-          >
-            {typeof value === 'number'
-              ? option
-              : option === 'all'
-              ? 'All Stages'
-              : option}
+        {options.length === 0 || isStageOptionsEmpty ? (
+          <option value="" disabled className="text-gray-700">
+            Nothing available
           </option>
-        ))}
+        ) : (
+          options.map((option, index) => (
+            <option
+              key={index}
+              value={typeof value === 'number' ? index : option}
+              className="truncate text-gray-700"
+            >
+              {typeof value === 'number'
+                ? option
+                : option === 'all'
+                ? 'All Stages'
+                : option}
+            </option>
+          ))
+        )}
       </select>
     </div>
   )
